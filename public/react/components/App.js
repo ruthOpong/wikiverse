a132c3ffe25490385798499179915a90d4a74526
= <=>
2023-11-16 18:35:54 +0000

    wiki

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -7,6 +7,7 @@ import apiURL from '../api';
 export const App = () => {
 
 	const [pages, setPages] = useState([]);
+	const [isNewPage, setIsNewPage] = useState(false);
 
 	async function fetchPages(){
 		try {
@@ -22,10 +23,27 @@ export const App = () => {
 		fetchPages();
 	}, []);
 
+	const [newForm, setNewForm] = useState({title: "", content: "", name: "", email: "", tags:""});
+
+	function submitPage (){
+		console.log(newForm);
+	}
 	return (
 		<main>	
       <h1>WikiVerse</h1>
 			<h2>An interesting 📚</h2>
+			<button onClick = {() => setIsNewPage(!isNewPage)}>New Article</button>
+			<form id = 'addPage' style = {isNewPage ? {display: "block"} : {display: "none"}}>
+				<div>
+					<input type="text" placeholder="title" aria-label="title" onChange = {(e) => setNewForm(e.target.value)} value ={newForm.title}/>
+					<input type="text" placeholder="content" aria-label="content" onChange = {(e) => setNewForm(e.target.value)} value ={newForm.content}/>
+					<input type="text" placeholder="name" aria-label="name" onChange = {(e) => setNewForm(e.target.value)} value ={newForm.name}/>
+					<input type="text" placeholder="email" aria-label="email" onChange = {(e) => setNewForm(e.target.value)} value ={newForm.email}/>
+					<input type="text" placeholder="tags" aria-label="tags" onChange = {(e) => setNewForm(e.target.value)} value ={newForm.tags}/>
+				</div>
+				<button onClick = {submitPage}>submit</button>
+			</form>
+			
 			<PagesList pages={pages} />
 		</main>
 	)
--- public/react/components/Page.js
+++ public/react/components/Page.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+import apiURL from '../api';
 
 export const Page = (props) => {
+  const [article, setArticle] = useState({});
+
+  function slugify (string){
+    string = string.toLowerCase();
+    string = string.trim();
+    string = string.replace(/\s/g, '_');
+    console.log(string);
+    return string;
+  }
+
+  async function openPage(){
+    let slug = slugify(props.page.title);
+    const response = await fetch(`${apiURL}/wiki/${slug}`);
+    const data = await response.json();
+    setArticle([data]);
+    console.log(article);
+  }
 
   return <>
-    <h3>{props.page.title}</h3>
+    <h3><button onClick={openPage}>{props.page.title}</button></h3>
   </>
 } 
 	
